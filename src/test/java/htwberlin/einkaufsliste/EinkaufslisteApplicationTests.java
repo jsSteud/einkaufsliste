da2d6888b2fbb96694f61cad50a0c23d9588b853
jascha.steudler@gmail.com <jascha.steudler@gmail.com>
2023-04-04 13:19:40 +0200

    spring

---
+++ src/test/java/htwberlin/einkaufsliste/EinkaufslisteApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.einkaufsliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinkaufslisteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
